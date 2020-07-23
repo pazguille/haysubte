fede5423a2f5a347fd98168a38d746dc88ed3afc
Guille Paz <guille87paz@gmail.com>
2020-07-23 00:01:50 -0300

    Add trust proxy config.

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ const pages = require('./app');
 const app = express();
 
 /**
+ * Set express trust proxy
+ */
+app.set('trust proxy', true);
+
+/**
  * Set static directory
  */
 app.use(express.static(__dirname + '/public'));
